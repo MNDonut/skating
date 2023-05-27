50d3e2780e0b7eac61b3703dec329c5a754b76e7
Ihor Sokhan <61380547+MNDonut@users.noreply.github.com>
2023-05-27 15:47:11 +0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-gzrlnl&ejvld)d85)m2==wi%f+tqvw*7zlcg3lr1r4p^9b_w!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skating-cvis.onrender.com']
 
 
 # Application definition
